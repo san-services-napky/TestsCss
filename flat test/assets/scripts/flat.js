ce035636e83a597eedd9347b8b2cad976615ecd2
napky <bnapky@sanservices.hn>
2015-08-20 08:36:57 -0600

    sandals nav

--- flat test/assets/scripts/flat.js
+++ flat test/assets/scripts/flat.js
@@ -3,9 +3,7 @@ var badge = document.getElementById('test');
 badge.textContent = value;
 
 $("#test").on("click", function(){
-
     var content = $(this).text();
     content++;
     $(this).text(content);
-    console.log(content);
 });
